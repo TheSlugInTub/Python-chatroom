ed87fd136ec8d3c9223184619fe608b2b62b6cd4
TheSlugInTub <114581396+TheSlugInTub@users.noreply.github.com>
2022-09-30 01:03:12 +0500

    Both new clients and server files.

--- Server.py
+++ Server.py
@@ -1,20 +1,88 @@
 import socket
+import select
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999))
-server.listen()
+HEADER_LENGTH = 10
+
+IP = "192.168.0.102"
+PORT = 9999
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+sockets_list = [server_socket]
+
+clients = {}
+
+print(f'Listening for connections on {IP}:{PORT}...')
+
+def receive_message(client_socket):
+
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+        message_length = int(message_header.decode('utf-8').strip())
+
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+
+        return False
+
+while True:
+
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
 
-client, addr = server.accept()
+    for notified_socket in read_sockets:
 
+        if notified_socket == server_socket:
 
-done = False
+            client_socket, client_address = server_socket.accept()
 
-while not done:
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done = True
-    else:
-        print(msg)
-        
-    client.send(input("Message: ").encode('utf-8'))
+            user = receive_message(client_socket)
+
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+
+        else:
+
+            message = receive_message(notified_socket)
+
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+
+                # Remove from list for socket.socket()
+                sockets_list.remove(notified_socket)
+
+                # Remove from our list of users
+                del clients[notified_socket]
+
+                continue
+
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            for client_socket in clients:
+
+                if client_socket != notified_socket:
+
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+
+        sockets_list.remove(notified_socket)
+
+        del clients[notified_socket]
